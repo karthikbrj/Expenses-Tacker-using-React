5ee4895af9ce00de9a83a1c6618bbecee6a29789
karthik <karthikbncet@gmail.com>
2024-07-24 16:13:03 +0530

    expenses calculations

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -4,14 +4,12 @@ const Transaction = ({transaction}) => {
     const sign=transaction.amount<0?"-":'+';
   return (
     <div>
-      (
-      <li className="minus">
+      <li className={transaction.amount<0 ?"minus":"plus"}>
         {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
         <button className="delete-btn">x</button>
       </li>
-      )
     </div>
-  );
+  );    
 };
 
 export default Transaction;
